72ce4afddf8199d1a957a8ab699819afd4c62748
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-20 21:58:47 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var middleware = require('../middleware');
+var request = require('request');
 
+
+
+
+const API_KEY = '3WCCv5qHyjmsh6brG8k3D9d9VqK7p1gEKwgjsnacjNbHEsKfsa'
+var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/'
+
+// .header("X-Mashape-Key", "3WCCv5qHyjmsh6brG8k3D9d9VqK7p1gEKwgjsnacjNbHEsKfsa")
+
+var options = {
+	url: url,
+	headers: {
+		'X-Mashape-Key': API_KEY
+	}
+}
+
+function callback(error, response, body) {
+	if(!error && response.statusCode == 200) {
+		// console.log(body);
+		var parsedData = JSON.parse(body);
+		console.log(parsedData);
+		console.log(parsedData["author"])
+	}
+	return parsedData
+}
 
 router.get('/', function(req, res) {
-	res.render('index');
-})
+	request(options, function(error, response, body) {
+		if(!error && response.statusCode == 200) {
+			var parsedData = JSON.parse(body);
+			res.render('index', {data: parsedData});
+		}
+	});
+});
 
 module.exports = router;
